88433a018f3f0408dc7af4afbf96ea62950679c8
datdt <duongtiendat3103@gmail.com>
2023-03-17 00:13:07 +0700

    feat(): background_game

---
+++ game_pokemon/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container">
+    <header class="title">Game Pokemon</header>
+    <ControllerComponents class="space "/>
+  </div>
+</template>
+
+<script setup>
+import ControllerComponents from './components/ControllerComponent.vue'
+
+
+</script>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+.container {
+  min-width: 100vw;
+  min-height: 100vh;
+  background-image: url('./assets/images/background.jpg');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+
+  align-items: center;
+}
+
+.title {
+  background-image: linear-gradient(to right, #dcfbf3,#1c5880,#e76c03, brown);
+  color: white;
+  height: 10%;
+  text-align: center;
+  padding: 10px;
+  font-size: 24px;
+  font-family: cursive;
+  width: 100%;
+}
+.space{
+  margin-top: 3%;
+}
+
+</style>
---
+++ game_pokemon/src/components/ControllerComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container_control">
+    <el-button type="info">Start game</el-button>
+    <div class="space">
+      <el-radio-group v-model="radio1">
+        <el-radio type="success" label="1" size="large" border>4</el-radio>
+        <el-radio label="2" size="large" border>6</el-radio>
+        <el-radio label="3" size="large" border>8</el-radio>
+        <el-radio label="4" size="large" border>10</el-radio>
+      </el-radio-group>
+    </div>
+  </div>
+</template>
+<script setup>
+import {ref} from "vue";
+
+const radio1 = ref('1')
+
+</script>
+<style scoped>
+.container_control {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+
+</style>
